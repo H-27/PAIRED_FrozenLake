87c423136d717784274fccce29424939c3f91097
H-27 <Jenshuth@t-online.de>
2024-09-28 16:59:34 +0200

    added new direction networks

--- networks.py
+++ networks.py
@@ -1,72 +1,62 @@
 import tensorflow as tf
 
-class Direction_Actor_Network(tf.keras.Model):
-    def __init__(self, n_actions, map_dims):
-        # Environment input branch
-        self.conv_layer = tf.keras.layers.Conv2D(16, (3, 3), activation='relu')
-        self.flatten_env = tf.keras.layers.Flatten()
+class Actor_Network(tf.keras.Model):
 
-        # Direction input branch
-        self.dense_direction = tf.keras.layers.Dense(5, activation='relu')
+    def __init__(self, n_actions):
+        super(Actor_Network, self).__init__()
+        self.layer_single_conv = self.convolution = tf.keras.layers.Conv2D(16, kernel_size=(3,3), padding='valid', data_format='channels_first')
+        self.dense_direction = tf.keras.layers.Dense(5, activation=tf.nn.relu)
+        self.lstm = tf.keras.layers.LSTM(128, activation='tanh', recurrent_activation='sigmoid',)
+        self.dense_one = tf.keras.layers.Dense(32, activation=tf.nn.relu)#, kernel_initializer=tf.initializers.random_normal, bias_initializer=tf.initializers.zeros)
+        self.dense_two = tf.keras.layers.Dense(32, activation=tf.nn.relu)#, kernel_initializer=tf.initializers.random_normal, bias_initializer=tf.initializers.zeros)
+        self.layer_out = tf.keras.layers.Dense(units = n_actions, activation = tf.nn.softmax, use_bias=False)
+        self.flatten = tf.keras.layers.Flatten()
 
-        # Concatenate environment and direction features
-        self.concat_layer = tf.keras.layers.Concatenate()
-
-        # LSTM layer
-        self.lstm_layer = tf.keras.layers.LSTM(256)
-
-        # Shared fully connected layers
-        self.dense_one = tf.keras.layers.Dense(32, activation='relu')
-        self.dense_two = tf.keras.layers.Dense(32, activation='relu')
-
-        # Policy output
-        self.policy_output = tf.keras.layers.Dense(n_actions, activation='softmax', name='policy')# Assuming n_actions is the number of possible actions
-
-    def call(self, map, direction, training=True):
-        x_env = self.conv_layer(map)
-        x_env = self.flatten_env(x_env)
-
+    @tf.function
+    def call(self, map, position, direction, training=True, transpose=True):
+        if (transpose):
+            # transposes input from channel first to channel last
+            map = tf.transpose(map, perm = [0,2,3,1])
+        x = self.layer_single_conv(map)
+        x = self.flatten(x)
         x_d = self.dense_direction(direction)
-
-        x = self.concat_layer([x_env, x_d], -1)
-        x = self.lstm_layer(x)
+        x = tf.concat([x, x_d], -1)
+        x = tf.concat([x, position], -1)
+        x = tf.expand_dims(x, 1)
+        x = self.lstm(x)
         x = self.dense_one(x)
         x = self.dense_two(x)
-        return self.policy_output(x)
+        x = self.layer_out(x)
+        return x
 
-class Direction_Critic_Network(tf.keras.Model):
-    def __init__(self, n_actions, map_dims):
-        # Environment input branch
-        self.conv_layer = tf.keras.layers.Conv2D(16, (3, 3), activation='relu', input_shape=(map_dims[0], map_dims[1], 3))
-        self.flatten_env = tf.keras.layers.Flatten()
+class Critic_Network(tf.keras.Model):
 
-        # Direction input branch
-        self.dense_direction = tf.keras.layers.Dense(5, activation='relu')
+    def __init__(self):
+        super(Critic_Network, self).__init__()
+        self.layer_single_conv = self.convolution = tf.keras.layers.Conv2D(16, kernel_size=(3,3), padding='valid', data_format='channels_first')
+        self.dense_direction = tf.keras.layers.Dense(5, activation=tf.nn.relu)
+        self.lstm = tf.keras.layers.LSTM(128, activation='tanh', recurrent_activation='sigmoid',)
+        self.dense_one = tf.keras.layers.Dense(32, activation=tf.nn.relu)#, kernel_initializer=tf.initializers.random_normal, bias_initializer=tf.initializers.zeros)
+        self.dense_two = tf.keras.layers.Dense(32, activation=tf.nn.relu)#, kernel_initializer=tf.initializers.random_normal, bias_initializer=tf.initializers.zeros)
+        self.layer_out = tf.keras.layers.Dense(units = 1, activation = None, use_bias=False)
+        self.flatten = tf.keras.layers.Flatten()
 
-        # Concatenate environment and direction features
-        self.concat_layer = tf.keras.layers.Concatenate()
-
-        # LSTM layer
-        self.lstm_layer = tf.keras.layers.LSTM(256)
-
-        # Shared fully connected layers
-        self.dense_one = tf.keras.layers.Dense(32, activation='relu')
-        self.dense_two = tf.keras.layers.Dense(32, activation='relu')
-
-        # Value estimate output
-        self.value_output = tf.keras.layers.Dense(1, name='value')
-
-    def call(self, map, direction, training=True):
-        x_env = self.conv_layer(map)
-        x_env = self.flatten_env(x_env)
-
+    @tf.function
+    def call(self, map, position, direction, training=True, transpose=True):
+        if (transpose):
+            # transposes input from channel first to channel last
+            map = tf.transpose(map, perm = [0,2,3,1])
+        x = self.layer_single_conv(map)
+        x = self.flatten(x)
         x_d = self.dense_direction(direction)
-
-        x = self.concat_layer([x_env, x_d], -1)
-        x = self.lstm_layer(x)
+        x = tf.concat([x, x_d], -1)
+        x = tf.concat([x, position], -1)
+        x = tf.expand_dims(x, 1)
+        x = self.lstm(x)
         x = self.dense_one(x)
         x = self.dense_two(x)
-        return self.value_output(x)
+        x = self.layer_out(x)
+        return x
 
 
 class Conv_Network(tf.keras.Model):
